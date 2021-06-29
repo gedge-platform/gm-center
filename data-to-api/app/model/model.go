8a8bcae51f07a504ad9345f1b16bf2ed61d6270a
taking <consine2@gmail.com>
2021-06-29 10:04:37 +0000

    data-to-api update

---
+++ data-to-api/app/model/model.go
@@ -0,0 +1,42 @@
+package model
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Member struct {
+	gorm.Model
+	Num 				int 			`gorm:"primaryKey" json:"num"`
+	Id 					string 			`json:"id"`
+	Name 				string 			`json:"name"`
+	Email 				string 			`json:"email"`
+	Contact 			string 			`json:"contact"`
+	Description 		string 			`json:"description"`
+	Enabled 			bool 			`json:"enabled"`
+	Created_at 			*time.Time 		`json:"created_at"`
+	Logined_at 			*time.Time 		`json:"logined_at"`
+	RoleName 			string 			`json:"roleName"`
+	
+	// text    string `gorm:"unique" json:"text"`
+	// text bool   `json:"text"`
+	// text    []text `gorm:"ForeignKey:textID" json:"text"`
+  }
+
+func (m *Member) Enabled() {
+	m.Enabled = true
+}
+
+func (m *Member) Disabled() {
+	m.Enabled = false
+}
+
+
+// DBMigrate will create and migrate the tables, and then make the some relationships if necessary
+// func DBMigrate(db *gorm.DB) *gorm.DB {
+// 	db.AutoMigrate(&Project{}, &Task{})
+// 	db.Model(&Task{}).AddForeignKey("project_id", "projects(id)", "CASCADE", "CASCADE")
+// 	return db
+// }
---
+++ data-to-api/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+type Config struct {
+	DB *DBConfig
+}
+
+type DBConfig struct {
+	Dialect  string
+	Host     string
+	Port     int
+	Username string
+	Password string
+	Name     string
+	Charset  string
+}
+
+func GetConfig() *Config {
+	return &Config{
+		DB: &DBConfig{
+			Dialect:  "mysql",
+			Host:     "192.168.150.102",
+			Port:     12003,
+			Username: "gedge-readonly",
+			Password: "innogrid",
+			Name:     "gedge",
+			Charset:  "utf8",
+		},
+	}
+}

daa3f5a5a9e77aa7b7755b4ec36773bb5e064bb8
taking <operet1@pusan.ac.kr>
2021-08-11 18:16:25 +0900

    services updated

--- gmc_database_api_server/app/common/Util.go
+++ gmc_database_api_server/app/common/Util.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"encoding/gob"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"gmc_database_api_server/app/model"
 	"log"
 	"reflect"
 	"strings"
+	"time"
 
 	"github.com/tidwall/gjson"
 )
@@ -53,91 +56,6 @@ func ArrStringtoBytes(i []string) []byte {
 	return bs
 }
 
-func Filter(i string, path string) string {
-	parse := gjson.Parse(i)
-	Dat := parse.Get(path)
-	// Arr := parse.Get(path).Array()
-	len := len(parse.Get(path).Array())
-
-	if len > 0 {
-		// list
-		// log.Printf("[#36] Arr is %+v\n", Arr)
-		// err3 := json.Unmarshal([]byte(Arr[0].String()), &x)
-		// if err3 != nil {
-		// 	fmt.Printf("Error : %s\n", err3)
-		// }
-		// fmt.Println("[#35] is ", x)
-		return Dat.String()
-	} else {
-		return Dat.String()
-	}
-}
-
-func FilterbyUID(i string, kind string, findValue string) []string {
-	var multiStr []string
-
-	parse := gjson.Parse(i)
-	k := parse.Get("items").Array()
-	for num, _ := range k {
-		arr := k[num].Get("metadata.ownerReferences").Array()
-		if len(arr) > 0 {
-			for num2, _ := range arr {
-				if strings.Contains(arr[num2].Get("kind").String(), kind) == true && strings.Contains(arr[num2].Get("uid").String(), findValue) == true {
-
-					log.Println("[#4] data : ", k[num].String())
-					multiStr = append(multiStr, k[num].String())
-					// err := json.Unmarshal([]byte(k[num].String()), &jobOnly)
-					// if err != nil {
-					// 	panic(err)
-					// }
-					// jobList = append(jobList, jobOnly)
-				}
-			}
-		}
-	}
-
-	return multiStr
-}
-
-func FilterbyName(i string, kind string, findValue string) []string {
-	var multiStr []string
-
-	iData := FindData(i, "", "")
-
-	log.Println("iData is ", iData)
-	log.Println("findValue is ", findValue)
-
-	if v, ok := SearchNested(iData, findValue); ok {
-		// fmt.Printf("[#4] %+v\n", v)
-		fmt.Println("hello [34] ", strings.Contains(InterfaceToString(v), findValue))
-		fmt.Println("Key found")
-	} else {
-		fmt.Println("Key not found")
-	}
-
-	parse := gjson.Parse(i)
-	k := parse.Get("items").Array()
-	for num, _ := range k {
-		arr := k[num].Get("metadata.ownerReferences").Array()
-		if len(arr) > 0 {
-			for num2, _ := range arr {
-				if strings.Contains(arr[num2].Get("kind").String(), kind) == true && strings.Contains(arr[num2].Get("uid").String(), findValue) == true {
-
-					log.Println("[#4] data : ", k[num].String())
-					multiStr = append(multiStr, k[num].String())
-					// err := json.Unmarshal([]byte(k[num].String()), &jobOnly)
-					// if err != nil {
-					// 	panic(err)
-					// }
-					// jobList = append(jobList, jobOnly)
-				}
-			}
-		}
-	}
-
-	return multiStr
-}
-
 func FindData(data string, findPath, findValue string) interface{} {
 
 	log.Println("FindPath is ", findPath)
@@ -173,6 +91,94 @@ func FindData(data string, findPath, findValue string) interface{} {
 	return x
 }
 
+func FindDataStr(data string, findPath, findValue string) string {
+
+	log.Println("FindPath is ", findPath)
+	findPathCheck := strings.Compare(findPath, "") != 0
+	if findPathCheck {
+		// findkey 입력이 있을 경우
+		data = Filter(data, findPath)
+	} else {
+		// findkey 가 "" 일 경우
+
+	}
+
+	log.Println("findValue is ", findValue)
+	findValueCheck := strings.Compare(findValue, "") != 0
+	if findValueCheck {
+		// findValue 입력이 있을 경우
+		data = Finding(data, findValue)
+
+	} else {
+		// findValue 가 "" 일 경우
+	}
+
+	log.Println("최종 data is : ", data)
+	fmt.Println("type:", reflect.ValueOf(data).Type())
+
+	return data
+}
+
+// func FindDataArr(data string, findPath, uniq string) string {
+
+// 	log.Println("FindPath is ", findPath)
+// 	log.Println("findValue is ", compareValue)
+
+// 	findValueCheck := strings.Compare(compareValue, "") != 0
+// 	if findValueCheck {
+// 		// findValue 입력이 있을 경우
+// 		data = Finding(data, compareValue)
+
+// 	} else {
+// 		// findValue 가 "" 일 경우
+// 	}
+
+// 	log.Println("최종 data is : ", data)
+// 	fmt.Println("type:", reflect.ValueOf(data).Type())
+
+// 	return data
+// }
+
+// func FilterUniq(i string, uniq string) string {
+// 	parse := gjson.Parse(i)
+// 	Dat := parse.Get(path)
+// 	// Arr := parse.Get(path).Array()
+// 	len := len(parse.Get(path).Array())
+
+// 	if len > 0 {
+// 		// list
+// 		// log.Printf("[#36] Arr is %+v\n", Arr)
+// 		// err3 := json.Unmarshal([]byte(Arr[0].String()), &x)
+// 		// if err3 != nil {
+// 		// 	fmt.Printf("Error : %s\n", err3)
+// 		// }
+// 		// fmt.Println("[#35] is ", x)
+// 		return Dat.String()
+// 	} else {
+// 		return Dat.String()
+// 	}
+// }
+
+func Filter(i string, path string) string {
+	parse := gjson.Parse(i)
+	Dat := parse.Get(path)
+	// Arr := parse.Get(path).Array()
+	len := len(parse.Get(path).Array())
+
+	if len > 0 {
+		// list
+		// log.Printf("[#36] Arr is %+v\n", Arr)
+		// err3 := json.Unmarshal([]byte(Arr[0].String()), &x)
+		// if err3 != nil {
+		// 	fmt.Printf("Error : %s\n", err3)
+		// }
+		// fmt.Println("[#35] is ", x)
+		return Dat.String()
+	} else {
+		return Dat.String()
+	}
+}
+
 func Finding(i string, find string) string {
 	parse := gjson.Parse(i)
 	Dat := parse
@@ -206,4 +212,75 @@ func Transcode(in, out interface{}) {
 func InterfaceToString(i interface{}) string {
 	str := fmt.Sprintf("%v", i)
 	return str
+}
+
+func StringToMapInterface(i string) map[string]interface{} {
+	x := make(map[string]interface{})
+	if err := json.Unmarshal([]byte(i), &x); err != nil {
+		fmt.Printf("Error : %s\n", err)
+	}
+	return x
+}
+
+func InterfaceToTime(i interface{}) time.Time {
+	createTime := InterfaceToString(i)
+	timer, _ := time.Parse(time.RFC3339, createTime)
+	return timer
+}
+
+func GetModelRelatedList(params model.PARAMS) (interface{}, error) {
+
+	data, err := GetModel(params)
+	if err != nil {
+		return "", err
+	}
+
+	switch strings.ToLower(params.Kind) {
+	case "services":
+		params.Kind = "endpoints"
+		params.Name = InterfaceToString(FindData(data, "metadata", "name"))
+
+		if data, err := GetModel(params); err != nil {
+			return nil, err
+		} else {
+			PodData := FindData(data, "subsets.#.addresses", "")
+			splits := strings.SplitN(InterfaceToString(FindData(data, "subsets.#.addresses.0", "targetRef.name")), "-", 3)
+			log.Printf("Endpoints [%s] Data is %s \n", params.Name, data)
+			log.Printf("splits %s \n", splits)
+			podName := splits[0] + "-" + splits[1]
+
+			params.Kind = "replicasets"
+			params.Name = podName
+
+			if data, err := GetModel(params); err != nil {
+				return nil, err
+			} else {
+				log.Printf("replicasets [%s] Data is %s \n", params.Name, data)
+				params.Kind = "deployments"
+				params.Name = FindDataStr(data, "metadata.ownerReferences.0", "name")
+
+				if data, err := GetModel(params); err != nil {
+					return nil, err
+				} else {
+					var deployModel model.Deployment
+					Transcode(data, &deployModel)
+
+					services := model.SERVICELISTS{
+						Pods: PodData,
+						Deployments: model.SERVICEDEPLOYMENT{
+							Name:     InterfaceToString(FindData(data, "metadata", "name")),
+							UpdateAt: InterfaceToTime(FindData(data, "status.conditions", "lastUpdateTime")),
+						},
+					}
+					return services, nil
+				}
+			}
+		}
+
+	case "deployments":
+		// params2.Kind = "endpoints"
+		// params2.Name = InterfaceToString(FindData(data, "metadata", "name"))
+
+	}
+	return nil, errors.New("")
 }

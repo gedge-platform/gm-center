55a35a9299f40b739f42a5d83d77420b92ac75c0
keunwooo <ladgudcks@naver.com>
2021-09-02 10:25:14 +0900

    query

--- gmc_database_api_server/app/api/nowMonit.go
+++ gmc_database_api_server/app/api/nowMonit.go
@@ -21,7 +21,7 @@ var nowClusterMetric = map[string]string{
 
 var nowNamespaceMetric = map[string]string{
 	"namespace_cpu":       "round(sum(sum(irate(container_cpu_usage_seconds_total{job='kubelet',pod!='',image!='', $1}[5m]))by(namespace,pod,cluster))by(namespace,cluster),0.001)",
-	"namespace_memory":    "sum(sum(container_memory_rss{job='kubelet',pod!='',image!='', $1})by(namespace,pod,cluster))by(namespace,cluster)",
+	"namespace_memory":    "round(sum(sum(container_memory_rss{job='kubelet',pod!='',image!=''})by(namespace,pod,cluster))by(namespace,cluster)/1024/1024/1024,0.1)",
 	"namespace_pod_count": "count(count(container_spec_memory_reservation_limit_bytes{pod!='', $1})by(pod,cluster,namespace))by(cluster,namespace)",
 }
 

cf77649374df5c52edd42aa544c6d8a9634307a9
yby654 <yby654@naver.com>
2022-03-02 16:54:21 +0900

    pv

---
+++ gmc_api_gateway/app/api/pv.go
@@ -0,0 +1,111 @@
+package api
+
+import (
+	"fmt"
+	"gmc_api_gateway/app/common"
+	"gmc_api_gateway/app/model"
+	// "log"
+	"net/http"
+	// "github.com/tidwall/sjson"
+	"github.com/labstack/echo/v4"
+)
+
+// GetPvs godoc
+// @Summary Show app PVs
+// @Description get pv List
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} model.PV
+// @Header 200 {string} Token "qwerty"
+// @Router /pvs [get]
+func GetAllPVs(c echo.Context) error {
+	var pvs []model.PV
+	fmt.Printf("## PVs", pvs)
+	params := model.PARAMS{
+		Kind:      "persistentvolumes",
+		Name:      c.Param("name"),
+		Cluster:   c.QueryParam("cluster"),
+		Workspace: c.QueryParam("workspace"),
+		Project:   c.QueryParam("project"),
+		Method:    c.Request().Method,
+		Body:      responseBody(c.Request().Body),
+	}
+	data := GetModelList(params)
+	fmt.Printf("####Pod data confirm : %s", data)
+
+	for i, _ := range data {
+		pv := model.PV{
+			Name:              common.InterfaceToString(common.FindData(data[i], "metadata", "name")),
+			Capacity:         common.InterfaceToString(common.FindData(data[i], "spec", "capacity.storage")),
+			AccessMode:           common.InterfaceToArray(common.FindData(data[i], "spec", "accessModes")),
+			ReclaimPolicy: common.InterfaceToString(common.FindData(data[i], "spec", "persistentVolumeReclaimPolicy")),
+			Status:            common.InterfaceToString(common.FindData(data[i], "status", "phase")),
+			Claim:          common.FindData(data[i], "spec", "claimRef"),
+			StorageClass:             common.InterfaceToString(common.FindData(data[i], "spec", "storageClassName")),
+			VolumeMode :            common.InterfaceToString(common.FindData(data[i], "spec", "volumeMode")),
+			Cluster: common.InterfaceToString(common.FindData(data[i], "clusterName", "")),
+			CreateAt : common.InterfaceToTime(common.FindData(data[i], "metadata", "creationTimestamp")),
+		}
+		pvs = append(pvs, pv)
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"data": pvs,
+	})
+}
+
+
+func GetPV(c echo.Context) error {
+	var pvs []model.PV
+	fmt.Printf("## PVs", pvs)
+	params := model.PARAMS{
+		Kind:      "persistentvolumes",
+		Name:      c.Param("name"),
+		Cluster:   c.QueryParam("cluster"),
+		Workspace: c.QueryParam("workspace"),
+		Project:   c.QueryParam("project"),
+		Method:    c.Request().Method,
+		Body:      responseBody(c.Request().Body),
+	}
+
+	getData, err := common.DataRequest(params)
+	if err != nil {
+		common.ErrorMsg(c, http.StatusNotFound, err)
+		return nil
+	}
+
+	fmt.Printf("####PV data confirm : %s", getData)
+	pv := model.PV{
+				Name:              common.InterfaceToString(common.FindData(getData, "metadata", "name")),
+				Capacity:         common.InterfaceToString(common.FindData(getData, "spec", "capacity.storage")),
+				AccessMode:           common.InterfaceToArray(common.FindData(getData, "spec", "accessModes")),
+				ReclaimPolicy: common.InterfaceToString(common.FindData(getData, "spec", "persistentVolumeReclaimPolicy")),
+				Status:            common.InterfaceToString(common.FindData(getData, "status", "phase")),
+				Claim:          common.FindData(getData, "spec", "claimRef"),
+				StorageClass:             common.InterfaceToString(common.FindData(getData, "spec", "storageClassName")),
+				VolumeMode :            common.InterfaceToString(common.FindData(getData, "spec", "volumeMode")),
+				Cluster: c.QueryParam("cluster"),
+				CreateAt : common.InterfaceToTime(common.FindData(getData, "metadata", "creationTimestamp")),
+				Events: getCallEvent(params),
+			}
+	// for i, _ := range data {
+	// 	pv := model.PV{
+	// 		Name:              common.InterfaceToString(common.FindData(data[i], "metadata", "name")),
+	// 		Capacity:         common.InterfaceToString(common.FindData(data[i], "spec", "capacity.storage")),
+	// 		AccessMode:           common.InterfaceToArray(common.FindData(data[i], "spec", "accessModes")),
+	// 		ReclaimPolicy: common.InterfaceToString(common.FindData(data[i], "spec", "persistentVolumeReclaimPolicy")),
+	// 		Status:            common.InterfaceToString(common.FindData(data[i], "status", "phase")),
+	// 		Claim:          common.FindData(data[i], "spec", "claimRef"),
+	// 		StorageClass:             common.InterfaceToString(common.FindData(data[i], "spec", "storageClassName")),
+	// 		VolumeMode :            common.InterfaceToString(common.FindData(data[i], "spec", "volumeMode")),
+	// 		Cluster: common.InterfaceToString(common.FindData(data[i], "clusterName", "")),
+	// 		CreateAt : common.InterfaceToTime(common.FindData(data[i], "metadata", "creationTimestamp")),
+	// 	}
+	// 	pvs = append(pvs, pv)
+	// }
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"data": pv,
+	})
+}
+
---
+++ gmc_api_gateway/app/model/pv.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"time"
+)
+
+type PV struct {
+	Name        string           `json:"name"`
+	Capacity   string           `json:"capacity"`
+	AccessMode      []string `json:"accessMode"`
+	ReclaimPolicy         string     `json:"reclaimPolicy",`
+	Status    interface{}      `json:"status"`
+	Claim   interface{}        `json:"claim"`
+	StorageClass       string           `json:"storageClass"`
+	// Reason        []EVENT          `json:"events"`
+	VolumeMode string  `json:"volumeMode"`
+	Cluster string  `json:"cluster"`
+	// Workspace string `json:"workspace"`
+	CreateAt time.Time          `json:"createAt"`
+	Events  []EVENT          `json:"events"`
+}
+
+
+type PVs []PVs
+
+func (PV) TableName() string {
+	return "PV_INFO"
+}

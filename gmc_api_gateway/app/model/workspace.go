86905dabde16492284de057cc8c7887692a79faa
kimsh1006 <tmdgks1006@naver.com>
2022-03-11 15:07:20 +0900

    cluster post update

--- gmc_api_gateway/app/model/workspace.go
+++ gmc_api_gateway/app/model/workspace.go
@@ -6,10 +6,10 @@ import (
 
 type Workspace struct {
 	_id           primitive.ObjectID  `json:"objectId,omitempty" bson:"_id"`
-	Name          string              `json:"workspaceName,omitempty" bson:"workspaceName"`
-	Description   string              `json:"workspaceDescription,omitempty" bson:"workspaceDescription"`
-	Owner         primitive.ObjectID  `json:"workspaceOwner,omitempty" bson:"workspaceOwner"`
-	Creator       primitive.ObjectID  `json:"workspaceCreator,omitempty" bson:"workspaceCreator"`
+	Name          string              `json:"workspaceName,omitempty" bson:"workspaceName" validate:"required"`
+	Description   string              `json:"workspaceDescription,omitempty" bson:"workspaceDescription" validate:"required"`
+	Owner         primitive.ObjectID  `json:"workspaceOwner,omitempty" bson:"workspaceOwner" validate:"required"`
+	Creator       primitive.ObjectID  `json:"workspaceCreator,omitempty" bson:"workspaceCreator" validate:"required"`
 	Selectcluster []WorkspaceClusters `json:"selectCluster,omitempty" bson:"selectCluster"`
 }
 

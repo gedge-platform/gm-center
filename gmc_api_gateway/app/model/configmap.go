0697aa0c103c4eeee2008bb0e5994bae2fa2555e
Hanseul <hansl2249@naver.com>
2022-03-14 11:26:31 +0900

    serviceaccount

--- gmc_api_gateway/app/model/configmap.go
+++ gmc_api_gateway/app/model/configmap.go
@@ -4,7 +4,7 @@ type CONFIGMAP struct {
 	Name        string      `json:"name"`
 	NameSpace   string      `json:"namespace"`
 	Cluster     string      `json:"cluster"`
-	Data        string      `json:"data ,omitempty"`
+	Data        interface{} `json:"data ,omitempty"`
 	Annotations interface{} `json:"annotations,omitempty"`
 	DataCnt     int         `json:"dataCnt"`
 	CreateAt    string      `json:"createAt"`

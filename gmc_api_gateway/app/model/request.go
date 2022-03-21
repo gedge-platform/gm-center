1800b6cb592cea82e57df2f261fa134a4af17e79
kimsh1006 <tmdgks1006@naver.com>
2022-03-21 17:48:47 +0900

    request get, post, put, delete 수정

--- gmc_api_gateway/app/model/request.go
+++ gmc_api_gateway/app/model/request.go
@@ -9,13 +9,13 @@ type Request struct {
 	Id 			string             `json:"requestId,omitempty" bson:"requestId" validate:"required"`
 	Status      string             `json:"status,omitempty" bson:"status"`
 	Message     string             `json:"message,omitempty" bson:"message"`
-	Workspace   primitive.ObjectID             `json:"workspace,omitempty" bson:"workspace" validate:"required"`
-	Project     primitive.ObjectID             `json:"project,omitempty" bson:"project" validate:"required"`
-	Date        primitive.DateTime `json:"date,omitempty" bson:"date"`
-	Cluster     primitive.ObjectID`json:"cluster,omitempty" bson:"cluster" validate:"required"`
+	Workspace   primitive.ObjectID `json:"workspace,omitempty" bson:"workspace" validate:"required"`
+	Project     primitive.ObjectID `json:"project,omitempty" bson:"project" validate:"required"`
+	Date        primitive.DateTime `json:"date,omitempty" bson:"date" validate:"required"`
+	Cluster     primitive.ObjectID `json:"cluster,omitempty" bson:"cluster" validate:"required"`
 	Name        string             `json:"name,omitempty" bson:"name" validate:"required"`
 	Reason      string             `json:"reason,omitempty" bson:"reason" validate:"required"`
-	// Type        string             `json:"type,omitempty" bson:"type" validate:"required"`
+	Type        string             `json:"type,omitempty" bson:"type"`
 }
 
 type RequestUpdate struct {
@@ -23,11 +23,11 @@ type RequestUpdate struct {
 	Id 			string             `json:"requestId,omitempty" bson:"requestId"`
 	Status      string             `json:"status,omitempty" bson:"status" validate:"required"`
 	Message     string             `json:"message,omitempty" bson:"message"`
-	Workspace   primitive.ObjectID             `json:"workspace,omitempty" bson:"workspace"`
-	Project     primitive.ObjectID             `json:"project,omitempty" bson:"project"`
+	Workspace   primitive.ObjectID `json:"workspace,omitempty" bson:"workspace"`
+	Project     primitive.ObjectID `json:"project,omitempty" bson:"project"`
 	Date        primitive.DateTime `json:"date,omitempty" bson:"date" validate:"required"`
-	Cluster     primitive.ObjectID`json:"cluster,omitempty" bson:"cluster"`
+	Cluster     primitive.ObjectID `json:"cluster,omitempty" bson:"cluster"`
 	Name        string             `json:"name,omitempty" bson:"name"`
 	Reason      string             `json:"reason,omitempty" bson:"reason"`
-	// Type        string             `json:"type,omitempty" bson:"type"`
+	Type        string             `json:"type,omitempty" bson:"type"`
 }
